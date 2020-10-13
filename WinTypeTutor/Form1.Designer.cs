5eb5b6aee5223e4762b754688254b395e530e8f3
GDC6 <GDC6@DESKTOP-66QNA8Q>
2020-10-13 18:22:48 +0900

    초기 커밋.

---
+++ WinTypeTutor/Form1.Designer.cs
@@ -0,0 +1,216 @@
+﻿namespace WinTypeTutor
+{
+	partial class Form1
+	{
+		/// <summary>
+		/// 필수 디자이너 변수입니다.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// 사용 중인 모든 리소스를 정리합니다.
+		/// </summary>
+		/// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form 디자이너에서 생성한 코드
+
+		/// <summary>
+		/// 디자이너 지원에 필요한 메서드입니다. 
+		/// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.components = new System.ComponentModel.Container();
+			System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+			this.groupBox1 = new System.Windows.Forms.GroupBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.lblScore = new System.Windows.Forms.Label();
+			this.groupBox2 = new System.Windows.Forms.GroupBox();
+			this.lblChance = new System.Windows.Forms.Label();
+			this.label4 = new System.Windows.Forms.Label();
+			this.progressBar1 = new System.Windows.Forms.ProgressBar();
+			this.lblSentence = new System.Windows.Forms.Label();
+			this.txtAnswer = new System.Windows.Forms.TextBox();
+			this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+			this.시작ToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+			this.timer1 = new System.Windows.Forms.Timer(this.components);
+			this.시작ToolStripMenuItem1 = new System.Windows.Forms.ToolStripMenuItem();
+			this.groupBox1.SuspendLayout();
+			this.groupBox2.SuspendLayout();
+			this.menuStrip1.SuspendLayout();
+			this.SuspendLayout();
+			// 
+			// groupBox1
+			// 
+			this.groupBox1.Controls.Add(this.lblScore);
+			this.groupBox1.Controls.Add(this.label1);
+			this.groupBox1.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.groupBox1.Location = new System.Drawing.Point(50, 44);
+			this.groupBox1.Name = "groupBox1";
+			this.groupBox1.Size = new System.Drawing.Size(300, 100);
+			this.groupBox1.TabIndex = 0;
+			this.groupBox1.TabStop = false;
+			this.groupBox1.Text = "점수";
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Font = new System.Drawing.Font("굴림체", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label1.Location = new System.Drawing.Point(54, 40);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(51, 19);
+			this.label1.TabIndex = 0;
+			this.label1.Text = "점수";
+			// 
+			// lblScore
+			// 
+			this.lblScore.AutoSize = true;
+			this.lblScore.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lblScore.Location = new System.Drawing.Point(154, 40);
+			this.lblScore.Name = "lblScore";
+			this.lblScore.Size = new System.Drawing.Size(81, 19);
+			this.lblScore.TabIndex = 1;
+			this.lblScore.Text = "lblScore";
+			// 
+			// groupBox2
+			// 
+			this.groupBox2.Controls.Add(this.label4);
+			this.groupBox2.Controls.Add(this.lblChance);
+			this.groupBox2.Font = new System.Drawing.Font("굴림체", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.groupBox2.Location = new System.Drawing.Point(428, 44);
+			this.groupBox2.Name = "groupBox2";
+			this.groupBox2.Size = new System.Drawing.Size(300, 100);
+			this.groupBox2.TabIndex = 1;
+			this.groupBox2.TabStop = false;
+			this.groupBox2.Text = "남은기회";
+			// 
+			// lblChance
+			// 
+			this.lblChance.AutoSize = true;
+			this.lblChance.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lblChance.Location = new System.Drawing.Point(72, 40);
+			this.lblChance.Name = "lblChance";
+			this.lblChance.Size = new System.Drawing.Size(96, 19);
+			this.lblChance.TabIndex = 0;
+			this.lblChance.Text = "lblChance";
+			// 
+			// label4
+			// 
+			this.label4.AutoSize = true;
+			this.label4.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.label4.Location = new System.Drawing.Point(174, 40);
+			this.label4.Name = "label4";
+			this.label4.Size = new System.Drawing.Size(29, 19);
+			this.label4.TabIndex = 1;
+			this.label4.Text = "회";
+			// 
+			// progressBar1
+			// 
+			this.progressBar1.Location = new System.Drawing.Point(46, 184);
+			this.progressBar1.Name = "progressBar1";
+			this.progressBar1.Size = new System.Drawing.Size(682, 35);
+			this.progressBar1.TabIndex = 2;
+			// 
+			// lblSentence
+			// 
+			this.lblSentence.AutoSize = true;
+			this.lblSentence.Font = new System.Drawing.Font("굴림", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.lblSentence.Location = new System.Drawing.Point(46, 253);
+			this.lblSentence.Name = "lblSentence";
+			this.lblSentence.Size = new System.Drawing.Size(141, 24);
+			this.lblSentence.TabIndex = 3;
+			this.lblSentence.Text = "lblSentence";
+			// 
+			// txtAnswer
+			// 
+			this.txtAnswer.Font = new System.Drawing.Font("나눔고딕", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+			this.txtAnswer.ImeMode = System.Windows.Forms.ImeMode.Hangul;
+			this.txtAnswer.Location = new System.Drawing.Point(46, 292);
+			this.txtAnswer.Name = "txtAnswer";
+			this.txtAnswer.Size = new System.Drawing.Size(682, 35);
+			this.txtAnswer.TabIndex = 0;
+			this.txtAnswer.TextChanged += new System.EventHandler(this.txtAnswer_TextChanged);
+			this.txtAnswer.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtAnswer_KeyPress);
+			// 
+			// menuStrip1
+			// 
+			this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem});
+			this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+			this.menuStrip1.Name = "menuStrip1";
+			this.menuStrip1.Size = new System.Drawing.Size(800, 24);
+			this.menuStrip1.TabIndex = 5;
+			this.menuStrip1.Text = "menuStrip1";
+			// 
+			// 시작ToolStripMenuItem
+			// 
+			this.시작ToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.시작ToolStripMenuItem1});
+			this.시작ToolStripMenuItem.Name = "시작ToolStripMenuItem";
+			this.시작ToolStripMenuItem.Size = new System.Drawing.Size(43, 20);
+			this.시작ToolStripMenuItem.Text = "시작";
+			// 
+			// timer1
+			// 
+			this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+			// 
+			// 시작ToolStripMenuItem1
+			// 
+			this.시작ToolStripMenuItem1.Name = "시작ToolStripMenuItem1";
+			this.시작ToolStripMenuItem1.Size = new System.Drawing.Size(180, 22);
+			this.시작ToolStripMenuItem1.Text = "시작";
+			this.시작ToolStripMenuItem1.Click += new System.EventHandler(this.시작ToolStripMenuItem1_Click);
+			// 
+			// Form1
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 12F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(800, 389);
+			this.Controls.Add(this.txtAnswer);
+			this.Controls.Add(this.lblSentence);
+			this.Controls.Add(this.progressBar1);
+			this.Controls.Add(this.groupBox2);
+			this.Controls.Add(this.groupBox1);
+			this.Controls.Add(this.menuStrip1);
+			this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+			this.MainMenuStrip = this.menuStrip1;
+			this.Name = "Form1";
+			this.Text = "Form1";
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.groupBox1.ResumeLayout(false);
+			this.groupBox1.PerformLayout();
+			this.groupBox2.ResumeLayout(false);
+			this.groupBox2.PerformLayout();
+			this.menuStrip1.ResumeLayout(false);
+			this.menuStrip1.PerformLayout();
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.GroupBox groupBox1;
+		private System.Windows.Forms.Label lblScore;
+		private System.Windows.Forms.Label label1;
+		private System.Windows.Forms.GroupBox groupBox2;
+		private System.Windows.Forms.Label label4;
+		private System.Windows.Forms.Label lblChance;
+		private System.Windows.Forms.ProgressBar progressBar1;
+		private System.Windows.Forms.Label lblSentence;
+		private System.Windows.Forms.TextBox txtAnswer;
+		private System.Windows.Forms.MenuStrip menuStrip1;
+		private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem;
+		private System.Windows.Forms.Timer timer1;
+		private System.Windows.Forms.ToolStripMenuItem 시작ToolStripMenuItem1;
+	}
+}
+
---
+++ WinTypeTutor/Form1.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace WinTypeTutor
+{
+	public partial class Form1 : Form
+	{
+		List<string> listSentence = new List<string>();
+		int chance = 10; //초기기회 10번
+		int score = 0; 
+		int scoreAdd = 200; // 한문제 맞출때마다 200점씩 추가
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Load(object sender, EventArgs e)
+		{
+			//메뉴를 클릭할때마다 파일을 읽어오는 것은 지금은 상관없지만 데이터가 커지면 소모가 너무커짐
+			//그래서 Load에 코딩하는게 좋음
+
+			//파일을 읽어서 List변수에 저장
+			StreamReader sr = new StreamReader("data/typedata.txt"); //절대경로 말고 상대경로로 하는게 좋다
+			// 1. 폴더를 추가하고 파일을 폴더에 넣는다
+			// 2. 폴더에 넣은 파일의 속성에서 <출력 디렉토리에 복사> 를 항상 복사로 바꿔준다
+			string temp;
+			while((temp = sr.ReadLine()) != null)
+			{
+				listSentence.Add(temp);
+			}
+			sr.Close();
+
+			//컨트롤의 초기화
+			//lblScore.Text = score.ToString();
+			//lblChance.Text = chance.ToString();
+			//lblScore.Text = "";
+			lblScore.Text = lblChance.Text = lblScore.Text = "";
+			//처음에 아무것도 출력안한 상태로 만들고 싶을때 이렇게 한줄로가능
+			txtAnswer.Enabled = false;
+
+
+		}
+
+		private void 시작ToolStripMenuItem1_Click(object sender, EventArgs e)
+		{
+			//progressBar컨트롤 값을 100으로 초기화 
+			//문제를 난수발생으로 뽑아서 라벨에 보여주기
+			//문제를 쓰다가 지나가면 텍스트 박스 초기화
+			//이것을 메서드로 통일
+			DisplaySentence();
+			
+		}
+
+		private void DisplaySentence()
+		{
+			//progressBar컨트롤 값을 100으로 초기화 
+			progressBar1.Value = 100;
+
+			//문제를 난수발생으로 뽑아서 라벨에 보여주기
+			Random rnd = new Random();
+			int i = rnd.Next(0, listSentence.Count);
+			lblSentence.Text = listSentence[i];
+			
+			//컨트롤 초기화
+			lblScore.Text = score.ToString();
+			lblChance.Text = chance.ToString();
+
+			//문제를 쓰다가 지나가면 텍스트 박스 초기화
+			txtAnswer.Enabled = true;
+
+			//txtAnswer.ImeMode = ImeMode.Hangul; // 속성으로 주지않고 코딩으로 주는방법
+			//txtAnswer.Text = "";
+			txtAnswer.Clear();//속성 메서드 사용해도 가능
+			txtAnswer.Select(0, 0);//텍스트박스의 텍스트 범위 설정
+			txtAnswer.Focus();//포커스 설정 
+			//Focus 커서만 그냥 가져다주는 역할
+			//글씨가 입력중에 넘어가면 버퍼에 쓰던게 남아있기때문에 Select메서드를 이용해 0,0으로 범위설정해줘서 해결(미해결)
+
+			timer1.Stop();// 문제를 입력하지 않아도 시간이 계속 가는 상황을 해결하기 위해
+		}
+
+		private void txtAnswer_TextChanged(object sender, EventArgs e)
+		{
+			//텍스트박스에 문제를 입력할때부터 시간이 가는 이벤트 등록
+			timer1.Interval = 1000; //1000이 1초
+			timer1.Start();
+		}
+
+		private void txtAnswer_KeyPress(object sender, KeyPressEventArgs e)
+		{
+			//엔터 입력시의 상황
+			if(e.KeyChar == 13) // 13이 엔터
+			{
+				timer1.Stop();
+				if(lblSentence.Text.Equals(txtAnswer.Text))
+				{
+					//문제를 맞혔을시
+					score += scoreAdd;
+				}
+				else
+				{
+					//틀렸을시
+					chance--;
+					if(chance == 0)
+					{
+						//MessageBox.Show("게임 종료");
+
+						//종료되었을시 재시작하는지 물어보기위한 코드
+						DialogResult result = MessageBox.Show("게임이 종료되었습니다. 다시 하시겠습니까?", "재시작 확인", MessageBoxButtons.YesNo);
+						if( result == DialogResult.Yes)
+						{
+							score = 0;
+							chance = 10;
+							DisplaySentence();
+						}
+						else
+						{
+							return;
+						}
+						//return; // 기회를 모두 소진하면 다음문제를 보여주지 않기 위해
+					}
+				}
+				//맞히거나 틀리거나 다음문제를 보여줘야하기때문에 조건문 밖에서 호출
+				DisplaySentence();
+			}
+		}
+
+		private void timer1_Tick(object sender, EventArgs e)
+		{
+			//반드시 프로그레스바의 밸류가 0아래로 내려갔을때의 상황 추가
+			if (progressBar1.Value < 10)
+			{
+				progressBar1.Value = 0;
+				timer1.Stop();
+
+				chance--; // 시간 다지날때까지 못풀었다는거니까 찬스하나 빼기
+				if (chance == 0)
+				{
+					//위에 KeyPress의 코드와 같이
+					DialogResult result = MessageBox.Show("게임이 종료되었습니다. 다시 하시겠습니까?", "재시작 확인", MessageBoxButtons.YesNo);
+					if (result == DialogResult.Yes)
+					{
+						score = 0;
+						chance = 10;
+						DisplaySentence();
+					}
+					else
+					{
+						return;
+					}
+				}
+				else
+				{
+					DisplaySentence();
+				}
+			}
+			else
+			{
+				progressBar1.Value -= 10;
+			}
+		}
+	}
+}
